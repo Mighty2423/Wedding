3c37a8cda38c97938ce70f78eb0b3a27e5a083f0
Mighty2423 <whi23062@byui.edu>
2024-08-07 12:51:09 -0600

    look over and arrow key added

--- ours/scripts.js
+++ ours/scripts.js
@@ -10,19 +10,35 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    let i;
     let slides = document.getElementsByClassName("mySlides");
     let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
+    for (let i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "ArrowLeft") {
+        plusSlides(-1);
+    } else if (event.key === "ArrowRight") {
+        plusSlides(1);
+    }
+});
+
+
+
+
+
+
+
+
+
 
 
